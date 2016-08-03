b791bf80e93362188402f06a3eed9ebe371b3791
Tim Bowden (c) <t.bowden@avanade.com>
2016-08-03 13:08:01 +0100

    commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>{{title}}</h1>
+                <div [style.color]='nameColor'>Hello {{name}}</div>
+                <button (click)='changeColor()'>Change Color</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+    title = 'Meros Angular 2 App';
+    name = 'Tim';
+    nameColor = 'red';
+
+    changeColor(){
+        this.nameColor = this.nameColor === 'red' ? 'green' : 'red';        
+    }
+}
