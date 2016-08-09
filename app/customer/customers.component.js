a43e847a8644fd2ffd3d117377df1b3f03a34473
Tim Bowden (c) <t.bowden@avanade.com>
2016-08-09 16:17:30 +0100

    Added error handling

--- app/customer/customers.component.js
+++ app/customer/customers.component.js
@@ -9,6 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Rx_1 = require('rxjs/Rx');
 var customer_component_1 = require('./customer.component');
 var customer_service_1 = require('./customer.service');
 var CustomersComponent = (function () {
@@ -16,7 +17,11 @@ var CustomersComponent = (function () {
         this._customerService = _customerService;
     }
     CustomersComponent.prototype.ngOnInit = function () {
-        this.customers = this._customerService.getCustomers();
+        this.customers = this._customerService.getCustomers()
+            .catch(function (err) {
+            console.log(err);
+            return Rx_1.Observable.of(function () { return true; });
+        });
     };
     CustomersComponent = __decorate([
         core_1.Component({
